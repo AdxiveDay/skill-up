fbbe88d38a5b94c7083017f7969e576c4d4b10c0
AdxiveDay <kaokongxddd@gmail.com>
2025-12-30 21:45:31 +0700

    credit lastone seeu

---
+++ app/credit/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+export default function CreditPage() {
+    return (
+        <main className="flex gap-4 flex-col justify-center h-screen items-center text-3xl">
+            <h1 className="font-black text-5xl animate-bounce">Creator</h1>
+            <h1>It's Khaokong!!</h1>
+            <p className="text-[#CDCDCD] text-md">IG: 15dxyy</p>
+        </main>
+    )
+}

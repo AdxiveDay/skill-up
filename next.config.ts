90a5c0b1dec2da729741c0b24102f5427dcb1f76
AdxiveDay <kaokongxddd@gmail.com>
2026-01-04 15:05:17 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
+import type { NextConfig } from "next";
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  api: {
-    bodyParser: {
-      sizeLimit: '100mb', // เปลี่ยนจาก default 1mb เป็น 100mb
-    },
-  },
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
